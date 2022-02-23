33af0d4936baf492cfd24656b9fecd26fb6ed9f5
Rabiul Islam <rabiulislam993@live.com>
2022-02-23 13:37:45 +0600

    added dummy data json

--- employee/views.py
+++ employee/views.py
@@ -2,6 +2,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated
 
 from employee.filters import EmployeeFilterSet
 from employee.models import Position, Employee
@@ -16,6 +17,7 @@ class PositionViewSet(viewsets.ModelViewSet):
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Employee.objects.all().order_by('position').select_related('position')
     serializer_class = EmployeeSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
